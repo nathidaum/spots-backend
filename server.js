05f11368d961e7ce937929d50c68d5494d4c0da4
nathidaum <daumnathi@gmail.com>
2024-11-16 11:55:26 +0100

    Initial setup: Express server, MongoDB connection, environment variables

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config(); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express(); 
+
+// Set up Middleware 
+app.use(express.json());
+app.use(cors())
+
+// Connect to MongoDB 
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => {console.log("COnnected to MongoDB")})
+.catch((error) => {console.log("Error connecting to MongoDB:", error)})
+
+// Test Route
+app.get("/", (req, res, next) => {
+    res.send("Welcome to the Spots API")
+}); 
+
+// Start Server 
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)}); 
