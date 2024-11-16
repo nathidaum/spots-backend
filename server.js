ecce3f9f3fd20699198458655c6cf2270cc89e2c
nathidaum <daumnathi@gmail.com>
2024-11-16 13:30:00 +0100

    Format file

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
-require("dotenv").config(); 
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express(); 
+const app = express();
 
-// Set up Middleware 
+// Set up Middleware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-// Connect to MongoDB 
+// Connect to MongoDB
 mongoose
-.connect(process.env.MONGO_URL)
-.then(() => {console.log("COnnected to MongoDB")})
-.catch((error) => {console.log("Error connecting to MongoDB:", error)})
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
 
 // Test Route
 app.get("/", (req, res, next) => {
-    res.send("Welcome to the Spots API")
-}); 
+  res.send("Welcome to the Spots API");
+});
 
-// Start Server 
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)}); 
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
