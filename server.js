2aefa8a01cd0b2c3ec3c0664ae0a4682d802f050
nathidaum <daumnathi@gmail.com>
2024-11-17 19:13:30 +0100

    Refactor: Move db connection in separate config file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -10,14 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB:", error);
-  });
+connectDB();
 
 // Handle routes
 app.use("/", require("./routes/user.routes"));
