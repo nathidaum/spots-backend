5e9ccd1fc23f92604fd48f5478b5172c6d44cc85
nathidaum <daumnathi@gmail.com>
2024-11-20 07:26:36 +0100

    Adjust CORS

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 
 // Set up Middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Connect to MongoDB
 connectDB();
